5edbe8122445a47a4e831b3aa8f691bd8598eaa4
Connor <55250388+Jaskaal@users.noreply.github.com>
2020-03-08 16:53:19 -0700

    Update document.R
    Managed to functionalize the file I/O, etc. to make it able to handle N data-sets, given that it doesn't cause overflow issues within R.
    By default it will only make two, but it reads in every set of data in case we want to set the default later.
    
    Default is set to 2 because of aforementioned issues with R allowing you to input any N data-sets, potentially causing memory issues.

--- document.R
+++ document.R
@@ -1,71 +1,68 @@
-##########################################################################################################
-# The purpose of this is to compare two 24-hour periods which is abstracted from the logfiles designated # 
-# as "in.txt" and "inSec.txt". The format will allow for 'x' amount of comparisons given 'x' is less     #
-# than a number that would simply convolute the graph further, making it unreadable.                     # 
-##########################################################################################################
+#################################################################################################
+# The purpose of this is to compare 'N' amount of 24-hour periods, which is extracted from the  #
+# logfiles designated in command line call as arguements. The format will allow for 'N'         #
+# amount of data sets. However, it should be noted that 'N' shall not exceed ~5, given that any #
+# more than that would simply convolute the graph further, making it unreadable.                #
+#################################################################################################
 
+# Import libraries
 library(ggplot2)
 library(tidyr)
 library(dplyr)
 
-# Designate files for I/O
-src <- file("in.txt", "rb")
-srcSec <- file("inSec.txt", "rb")
+# Define data collection function
+getInput <- function(times, input) {
+	# Designate file for I/O
+	src <- file(input, "rb")
 
-# Get line by line input from 'src' file
-count <- 1
-times <- c()
-while (length(cLine <- readLines(src, warn=FALSE, n=1)) > 0) {
-	# Split string into a vector
-	cList <- strsplit(cLine, " ")
+	# Get line by line input from 'input' file
+	count <- 1
+	data <- c()
+	while (length(cLine <- readLines(src, warn=FALSE, n=1)) > 0) {
+		# Split string into a vector
+		cList <- strsplit(cLine, " ")
 
-	# Check for event type, can be used as error checking
-	if (cList[[1]][9] == "ACTIVITY") {
-		cTime <- strsplit(cList[[1]][5], ":") 
-		cNum <- as.numeric(cTime[[1]][1]) + (as.numeric(cTime[[1]][2]) * 1/60) + (as.numeric(cTime[[1]][3]) * 1/3600)
-		times[count] <- cNum
-		count <- count + 1
+		# Check for event type
+		if (cList[[1]][9] == "ACTIVITY") {
+			# Insert converted data into our vector and increment the counter
+			cTime <- strsplit(cList[[1]][5], ":") 
+			data[count] <-  (as.numeric(cTime[[1]][1]) +          # Hours
+							(as.numeric(cTime[[1]][2]) * 1/60) +  # Minutes
+							(as.numeric(cTime[[1]][3]) * 1/3600)) # Seconds
+			count <- count + 1
+		}
 	}
-}
-close(src)
+	close(src)
+	return(data)
+}	
 
-# Get secondary input
-timesSec <- c()
-count <- 1
-while (length(cLine <- readLines(srcSec, warn=FALSE, n=1)) > 0) {
-	# Split string into a vector
-	cList <- strsplit(cLine, " ")
-
-	# Check for event type ('ACTIVITY' VS 'ERROR')
-	if (cList[[1]][9] == "ACTIVITY") {
-		# Insert converted data (cList[[1]][5]) into our vector and increment the counter for vector indexing
-		cTime <- strsplit(cList[[1]][5], ":") 
-		timesSec[count] <- as.numeric(cTime[[1]][1]) + (as.numeric(cTime[[1]][2]) * 1/60) + (as.numeric(cTime[[1]][3]) * 1/3600)
-		count <- count + 1
-	}
+# Get data from command line arguments
+args = commandArgs(trailingOnly = TRUE)
+times <- list()
+for (i in 1:length(args)) {
+	times[[i]] <- getInput(times, args[i])
 }
-close(srcSec)
-  
+
 # PDF/Plot generation
 pdf("activity-plot.pdf")
 
-# Set the base DF and the DF for mean values
-df <- data.frame("{DATE 1}"=sample(times, length(times), replace = FALSE), "{DATE 2}"=sample(timesSec, length(timesSec), replace = FALSE))
+# Set the base DF and the DF for mean values (Only allowing 2 24-hr comparisons by default)
+df <- data.frame("{DATE 1}"=sample(times[[1]], length(times[[1]]), replace = FALSE), "{DATE 2}"=sample(times[[2]], length(times[[2]]), replace = FALSE))
 df_lab <- df %>% pivot_longer(everything(), names_to = "var",values_to = "val") %>% group_by(var) %>% summarise(Mean = mean(val), Density = max(density(val)$y))
 
-# Create the graph
+# Create the graph by passing in the DF and adding to the information sequentially
 df %>% pivot_longer(everything(), names_to = "var", values_to = "val") %>%
 	# Base graph info
 	ggplot(aes(x = val, fill = var, colour = var)) +
 	geom_density(alpha = 0.8) +
 	theme(legend.position="top") +
 
-	# Data Set 2 MISC.
+	# Data set 2 colourings
 	scale_color_manual(values = c("#4271AE", "#FF7F00")) +
 	scale_fill_manual(values = c("#1A3552", "#D3D3D3")) +
 
-	# Mean vertical lines & assoc. text
-	geom_vline(inherit.aes = FALSE, data = df_lab, aes(xintercept = Mean, color = var), linetype = "dashed", size = 1, show.legend = FALSE) +
+	# Mean grouping vertical lines
+	geom_vline(data = df_lab, aes(xintercept = Mean, color = var), linetype = "dashed", size = 1, show.legend = FALSE) +
 	geom_text(inherit.aes = FALSE, data = df_lab, aes(x = Mean-0.5, y = Density/2, label = var, color = var), angle = 90, show.legend = TRUE) +
 
 	# X-Y axis & Labels
