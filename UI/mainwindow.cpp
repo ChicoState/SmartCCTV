f2532f8dbec640a9dfcee74b658ad0addd40eadc
Wei Tao Li <weitaoli123@gmail.com>
2020-03-25 11:40:08 -0700

    What I currently have for the UI

---
+++ UI/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_Statistics_clicked()
+{
+    ui->label->setText("Chart goes here depending on selected option.");
+}
