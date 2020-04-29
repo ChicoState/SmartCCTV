6623a2d2bf1b59de989e1f788dd1c8673adba459
SlavikChukhlebov <slavikchukhlebov@gmail.com>
2020-04-29 14:49:25 -0700

    Added preprocessor guard to humanFilter.hpp

--- humanFilter.hpp
+++ humanFilter.hpp
@@ -13,6 +13,7 @@
 
 #ifndef HUMANFILTER_HPP
 #define HUMANFILTER_HPP
+
 #include <opencv2/objdetect.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -21,15 +22,11 @@
 #include <vector>
 #include <iomanip>
 
-//using namespace cv;
-//using namespace std;
-
-
 class HumanFilter
 {
 public:
 	HumanFilter();
-    bool runRecognition(cv::Mat &frame);
+	bool runRecognition(cv::Mat &frame);
     
 private:
 	cv::HOGDescriptor hog;
