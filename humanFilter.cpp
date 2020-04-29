cccf93658ee7641f42bcc0dc1ae5e0c67c1239bc
SlavikChukhlebov <slavikchukhlebov@gmail.com>
2020-04-29 14:40:07 -0700

    Fixed the copy-paste error with humanFilter.cpp
    I accidentally copied the contents of humanFilter.hpp into .cpp. Now fixed.

--- humanFilter.cpp
+++ humanFilter.cpp
@@ -1,5 +1,5 @@
 /**
- * File Name:  humanFilter.hpp
+ * File Name:  humanFilter.cpp
  * Created By:  Svyatoslav Chukhlebov <slavikchukhlebov@mail.csuchico.edu>
  * Created On:  4/25/20
  *
@@ -11,26 +11,38 @@
  * Each instance of this class is to correspond to a single camera or video file.
  */
 
-#include <opencv2/objdetect.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
-#include <iostream>
-#include <vector>
-#include <iomanip>
+#include "humanFilter.hpp"
 
-//using namespace cv;
-//using namespace std;
-
-
-class HumanFilter
+HumanFilter::HumanFilter()
 {
-public:
-	HumanFilter();
-    bool runRecognition(cv::Mat &frame);
-    
-private:
-	cv::HOGDescriptor hog;
-	std::vector<cv::Rect> boxes;
-};
+	hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+}
+
+bool HumanFilter::runRecognition(cv::Mat &frame)
+{
+	boxes.clear();
+	//The third value is used to set detection threshold (higher = less false positives, more false negatives)
+	//Recommended value between 1.3 and 1.7
+	hog.detectMultiScale(frame, boxes, 1.7, cv::Size(8,8), cv::Size(), 1.05, 2, false);
 	
+	if(boxes.size() < 1)
+	{
+		return false;
+	}
+	
+	for (size_t i = 0; i < boxes.size(); i++)
+    {
+        cv::Rect &rect = boxes[i];
+        
+        rect.x += cvRound(rect.width*0.1);
+        rect.width = cvRound(rect.width*0.8);
+        rect.y += cvRound(rect.height*0.07);
+        rect.height = cvRound(rect.height*0.8);
+        
+        if(true) //placeholder- replace with if(daemon_data.enable_boxes) once daemon_data API is ready
+        {
+			rectangle(frame, rect.tl(), rect.br(), cv::Scalar(0, 255, 0), 2);
+		}
+    }
+	return true;
+}
