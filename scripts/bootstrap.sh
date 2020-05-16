02f523a03ed5d8b41b35e93894902a09e594a008
Krebrov001 <krebrov001@student.butte.edu>
2020-05-15 22:26:59 -0700

    Work in progress bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,21 +10,33 @@ cp Makefile_skeleton Makefile
 
 # Grep the info that's in the generated Makefile 
 # and store it into variables
-e=$(cat Makefile_qmake | grep "INCPATH       = ")
-f=$(cat Makefile_qmake | grep "LIBS          = ")
-g=$(cat Makefile_qmake | grep "moc_mainwindow.cpp: ")
-h=$(cat Makefile_qmake | grep "ui_mainwindow.h: ")
+e=$(sed -n '/INCPATH       = /,/^$/p' Makefile_qmake)
+f=$(sed -n '/LIBS          = /,/^$/p' Makefile_qmake)
+g=$(sed -n '/moc_mainwindow.cpp: /,/^$/p' Makefile_qmake)
+h=$(sed -n '/ui_mainwindow.h: /,/^$/p' Makefile_qmake)
 
 # Swap those stored line variables into the skeleton with sed()
 # by looking for lines we know will show up in the skeleton
-a="INCPATH       = -I. -isystem /usr/include/x86_64-linux-gnu/qt5 -isystem /usr/include/x86_64-linux-gnu/qt5/QtWidgets -isystem /usr/include/x86_64-linux-gnu/qt5/QtGui -isystem /usr/include/x86_64-linux-gnu/qt5/QtCore -I. -I. -I/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
-b="LIBS          = $(SUBLIBS) -L/usr/X11R6/lib64 -lQt5Widgets -lQt5Gui -lQt5Core -lGL -lpthread"
-c="moc_mainwindow.cpp: sources/high_level_cctv_daemon_apis.h \ sources/mainwindow.h /usr/lib/x86_64-linux-gnu/qt5/bin/moc $(DEFINES) -I/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64 -I/home/konstantin/Documents/programming/SmartCCTV -I/usr/include/x86_64-linux-gnu/qt5 -I/usr/include/x86_64-linux-gnu/qt5/QtWidgets -I/usr/include/x86_64-linux-gnu/qt5/QtGui -I/usr/include/x86_64-linux-gnu/qt5/QtCore -I/usr/include/c++/5 -I/usr/include/x86_64-linux-gnu/c++/5 -I/usr/include/c++/5/backward -I/usr/lib/gcc/x86_64-linux-gnu/5/include -I/usr/local/include -I/usr/lib/gcc/x86_64-linux-gnu/5/include-fixed -I/usr/include/x86_64-linux-gnu -I/usr/include sources/mainwindow.h -o moc_mainwindow.cpp"
-d="i_mainwindow.h: sources/mainwindow.ui /usr/lib/x86_64-linux-gnu/qt5/bin/uic sources/mainwindow.ui -o ui_mainwindow.h"
-sed -i 's|'"${a}"'|'"${e}"'|' Makefile
-sed -i 's|'"${b}"'|'"${f}"'|' Makefile
-sed -i 's|'"${c}"'|'"${g}"'|' Makefile
-sed -i 's|'"${d}"'|'"${h}"'|' Makefile
+a=$(sed -n '/INCPATH       = /,/^$/p' Makefile_skeleton)
+b=$(sed -n '/LIBS          = /,/^$/p' Makefile_skeleton)
+c=$(sed -n '/moc_mainwindow.cpp: /,/^$/p' Makefile_skeleton)
+d=$(sed -n '/ui_mainwindow.h: /,/^$/p' Makefile_skeleton)
+
+replacement1="s/'$(echo $a)'/'$(echo $e)'/"
+replacement2="s/'$(echo $b)'/'$(echo $f)'/"
+replacement3="s/'$(echo $c)'/'$(echo $g)'/"
+replacement4="s/'$(echo $d)'/'$(echo $h)'/"
+
+# Perform the actual repalcements
+sed -i $replacement1 Makefile
+#sed -i $replacement2 Makefile
+#sed -i $replacement3 Makefile
+#sed -i $replacement4 Makefile
+
+#sed -i 's|' "${a}" '|' "${e}" '|' Makefile
+#sed -i 's|'"${b}"'|'"${f}"'|' Makefile
+#sed -i 's|'"${c}"'|'"${g}"'|' Makefile
+#sed -i 's|'"${d}"'|'"${h}"'|' Makefile
 
 # Remove the temporary Makefile_qmake
 rm Makefile_qmake
