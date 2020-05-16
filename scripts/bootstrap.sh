8b063d75bdc14effa0139b1d1da457e1dca33f74
Krebrov001 <krebrov001@student.butte.edu>
2020-05-16 00:36:42 -0700

    This version of bootstrap.sh is working for me, uusing replace command

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,35 +8,24 @@ mv Makefile Makefile_qmake
 # having a backup of the original copy.
 cp Makefile_skeleton Makefile
 
-# Grep the info that's in the generated Makefile 
-# and store it into variables
-e=$(sed -n '/INCPATH       = /,/^$/p' Makefile_qmake)
-f=$(sed -n '/LIBS          = /,/^$/p' Makefile_qmake)
-g=$(sed -n '/moc_mainwindow.cpp: /,/^$/p' Makefile_qmake)
-h=$(sed -n '/ui_mainwindow.h: /,/^$/p' Makefile_qmake)
-
 # Swap those stored line variables into the skeleton with sed()
 # by looking for lines we know will show up in the skeleton
-a=$(sed -n '/INCPATH       = /,/^$/p' Makefile_skeleton)
-b=$(sed -n '/LIBS          = /,/^$/p' Makefile_skeleton)
-c=$(sed -n '/moc_mainwindow.cpp: /,/^$/p' Makefile_skeleton)
-d=$(sed -n '/ui_mainwindow.h: /,/^$/p' Makefile_skeleton)
+old_incpath=$(cat Makefile_skeleton | grep "INCPATH       = ")
+old_libs=$(cat Makefile_skeleton | grep "LIBS          = ")
+old_moc_mainwindow=$(sed -n '/moc_mainwindow.cpp:/,/^$/p' Makefile_skeleton)
+old_ui_mainwindow=$(sed -n '/ui_mainwindow.h:/,/^$/p' Makefile_skeleton)
 
-replacement1="s/'$(echo $a)'/'$(echo $e)'/"
-replacement2="s/'$(echo $b)'/'$(echo $f)'/"
-replacement3="s/'$(echo $c)'/'$(echo $g)'/"
-replacement4="s/'$(echo $d)'/'$(echo $h)'/"
+# Grep the info that's in the generated Makefile 
+# and store it into variables
+new_incpath=$(cat Makefile_qmake | grep "INCPATH       = ")
+new_libs=$(cat Makefile_qmake | grep "LIBS          = ")
+new_moc_mainwindow=$(sed -n '/moc_mainwindow.cpp:/,/^$/p' Makefile_qmake)
+new_ui_mainwindow=$(sed -n '/ui_mainwindow.h:/,/^$/p' Makefile_qmake)
 
-# Perform the actual repalcements
-sed -i $replacement1 Makefile
-#sed -i $replacement2 Makefile
-#sed -i $replacement3 Makefile
-#sed -i $replacement4 Makefile
-
-#sed -i 's|' "${a}" '|' "${e}" '|' Makefile
-#sed -i 's|'"${b}"'|'"${f}"'|' Makefile
-#sed -i 's|'"${c}"'|'"${g}"'|' Makefile
-#sed -i 's|'"${d}"'|'"${h}"'|' Makefile
+replace "$old_incpath" "$new_incpath" -- Makefile
+replace "$old_libs" "$new_libs" -- Makefile
+replace "$old_moc_mainwindow" "$new_moc_mainwindow" -- Makefile
+replace "$old_ui_mainwindow" "$new_ui_mainwindow" -- Makefile
 
 # Remove the temporary Makefile_qmake
 rm Makefile_qmake
